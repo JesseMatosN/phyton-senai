f6d4d87054a46ba8d874237727e73f3447d4bc16
JessematosN <jessematos2009@gamil.com>
2026-09-14 16:37:38 -0300

    adicionando arquivo

---
+++ Definicao-Literal/imprimir-conteudo.py
@@ -0,0 +1,8 @@
+# Quebra de linha
+print("Hoje tem semifinal: \nFrança vs espanha")
+
+# Aqui tem 3 parametros
+print("meu nome é", "jesse matos ", end="*")
+
+# Separador no print
+print("Meu", "nome", "é", "jesse", sep="*")
