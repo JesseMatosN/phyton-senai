258f899ee076d1cbfbbe73239847cebff8b8f5d0
JessematosN <jessematos2009@gamil.com>
2026-09-15 14:31:22 -0300

    feat

---
+++ Definicao-Literal/literais.py
@@ -0,0 +1,17 @@
+# String
+nome = "jesse"
+
+# Numeros inteiros e decimais.
+idade = 17
+altura = 1.80
+
+# Valor logico (True e False)
+professor = False
+
+# Verifica a tipagem das variaveis
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(professor))
+
+
