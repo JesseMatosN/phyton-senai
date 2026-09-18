2d13f4b58e395c0cc69dbf7e49276d1aedf11efd
JessematosN <jessematos2009@gamil.com>
2026-09-18 14:11:04 -0300

    operadores de comparação

---
+++ extrutura-de-decisao/01-peradores-comparacao.py
@@ -0,0 +1,24 @@
+# Entrada de dados
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+
+
+# Exibe o resultado da espressões
+
+# Verifica se os numeros são iguais
+print("n1 == n2 : ", numero1 == numero2, "\n")
+
+# Verifica se os numeros são diferentes
+print("n1 != n2: ", numero1 != numero2, " \n")
+
+# Verifica se o numero1 é menor que o numero2
+print("n1 < n2: ", numero1 < numero2, "\n")
+
+# Verifica se o numero1 é ou igual a numero2
+print("n1 <= n2 : ", numero1 <= numero2, "\n")
+
+# Verifica se o numero1 é maior que o numero2
+print("n1 > n2 : ", numero1 > numero2, "\n")
+
+# Verifica se o numero1 é maior ou igual o numero2
+print("n1 >= n2 : ", numero1 >= numero2, "\n")
