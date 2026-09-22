50c7523481fda7e2943e7011471425f87c267046
JessematosN <jessematos2009@gamil.com>
2026-09-22 16:42:49 -0300

    listas conc

---
+++ 05-listas/06-lista-agrupadas.py
@@ -0,0 +1,11 @@
+filmes = [
+    ["Batman", "Ação"],
+    ["Titanic", "Romance"],
+    ["Cidade de deus", "Toy story"]
+]
+
+# Cada filme agrupado com seu gênero
+
+print(filmes[0])
+print(filmes[0][0])
+print(filmes[0][1])
